cfffaa234e9ffbde2c8c5c91caf51ffa6ddfed6c
Nofacefreak <titusli64@yahoo.com>
2023-03-06 18:53:33 -0700

    before modify addCity

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,6 +11,30 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> cities;
+    /**
+     * this gets size of the list
+     * @return
+     */
+    public int getCount(){
+        return cities.size();
+    }
 
+    /**
+     * this adds a city object to the list
+     *for the first phase it will be empty
+     * @param city
+     */
+    public void addCity(City city){
+    }
 
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
 }
